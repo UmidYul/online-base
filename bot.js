c9d53930a729b74ba2b5d7407200b7dbe6370016
ScummerN1 <9a99y2705@gmail.com>
2023-10-03 12:12:11 +0500

    sadfasd

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 const app = express()
+const PORT = 3000
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const file = join(__dirname, 'db.json')
 const adapter = new JSONFile(file)
@@ -19,31 +20,49 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/views/main.html")
 })
-
 app.get('/login', function (req, res) {
     res.sendFile(__dirname + "/views/login.html")
 })
-app.get('/admin', function (req, res) {
-    res.send("admin")
+app.get('/admin:id', function (req, res) {
+    res.sendFile(__dirname + "/views/admin.html")
 })
-app.get('/teacher', function (req, res) {
+app.get('/teacher:id', function (req, res) {
     res.sendFile(__dirname + "/views/teacher.html")
 })
 app.post("/login-api", async function (req, res) {
     const { log, pass } = req.body
     await db.read()
-    const { logins } = db.data
+    const { logins, users } = db.data
     const user = logins.find(({ login }) => login == log)
-    if (pass === user.password) {
-        console.log(true)
-        if (user.role === "admin") {
-            res.redirect("/admin")
+    if (user) {
+        if (user.password === pass) {
+            for (let i = 0; i < users.length; i++) {
+                const el = users[i].info;
+                if (el.id == user.id) {
+                    if (el.role == "admin") {
+                        res.redirect(`/admin:${el.id}`)
+                    } else {
+                        res.redirect(`/teacher:${el.id}`)
+                    }
+                }
+            }
         } else {
-            res.redirect("/teacher")
+            res.redirect("/login")
         }
     } else {
         res.redirect("/login")
     }
 })
+app.post("/id", async function (req, res) {
+    const { data } = req.body
+    await db.read()
+    const { users } = db.data
+    for (let i = 0; i < users.length; i++) {
+        const el = users[i].info;
+        if (el.id == data) {
+            res.send(JSON.stringify(el))
+        }
+    }
+})
 
-app.listen(3000)
+app.listen(PORT, console.log(`http://localhost:${PORT}`))
